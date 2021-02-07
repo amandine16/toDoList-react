b1b16304db4b3a6c7b828069ac8989a711723188
Amandine <amandinedelavoie@live.fr>
2021-02-07 20:20:55 +0100

    fonctionnalités ok : ajout, suppression, deplacement des tasks checkés, recherche par titre

--- src/App.js
+++ src/App.js
@@ -4,52 +4,87 @@ import { useState } from "react";
 
 function App() {
   // ma liste initiale
-  const [maListe, setMaListe] = useState(["faire à manger", "nourir le chat"]);
+  const [maListe, setMaListe] = useState([]);
   // mon input initial
   const [input, setInput] = useState("");
-  // mon bouton check initial
-  const [check, setCheck] = useState(false);
-  // ma classe pour barré le texte
-  const [color, setColor] = useState("noLine");
+  // input de recherche initale
+  const [search, setSearch] = useState("");
+  // Les fonctions handle
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // je crée une copie de ma liste
+    const newListe = [...maListe];
+    //j'ajoute ma nouvelle tâche sous forme d'objet (le + récent en premier)
+    newListe.unshift({ task: input, check: false, display: "block" });
+    // Je modifie ma listeIntiale, par ma newListe
+    setMaListe(newListe);
+    // Remet à zéro l'input d'ajout
+    setInput("");
+  };
 
   return (
     <div className="App">
+      {/* formulaire de recherche */}
+      <form>
+        {/* input de recherche */}
+        <input
+          type="text"
+          placeholder="search"
+          value={search}
+          onChange={(e) => {
+            const value = e.target.value;
+            setSearch(value);
+            const newTab = [...maListe];
+            for (let i = 0; i < maListe.length; i++) {
+              if (newTab[i].task.indexOf(value) !== -1) {
+                console.log(newTab[i]);
+                newTab[i].display = "block";
+              } else {
+                newTab[i].display = "none";
+              }
+            }
+          }}
+        />
+      </form>
       {maListe.map((elem, index) => {
         /* Affiche les éléments de ma liste dynamiquement (boucle) */
-
         return (
           // Une tâche
           <div className="oneTask" key={index}>
             {/* Bouton Checkbox */}
-
             <input
               type="checkbox"
-              // checked={false}
-              onClick={(event) => {
-                // lors du click sur la checkbox, la valeur du check change pour faire changer l'aspect de la tache
-                !check ? setCheck(true) : setCheck(false);
-                check ? setColor("noLine") : setColor("line");
-                console.log(event);
+              checked={elem.check}
+              onChange={(event) => {
+                const newTab = [...maListe]; //[monTableauComplet]
+                let target = event.target.checked; //valeur de l'input checkbox
+                newTab[index].check = target; //cible l'objet actuel et check
+                const objetSup = newTab.splice(index, 1); //supprime l'objet actuel
+                newTab.push(objetSup[0]); //ajoute en derniere position l'objet actuel supprimé
+                setMaListe(newTab); //envoie dans maListe
               }}
             />
 
             {/* ma tâche  */}
-            <p className={color}>{elem}</p>
+            <p
+              className={elem.check ? "line" : "noLine"}
+              key={elem.key}
+              style={{ display: elem.display === "block" ? "block" : "none" }}
+            >
+              {elem.task}
+            </p>
 
             {/* bouton Delete */}
             <button
               onClick={() => {
-                // quand je clique sur la corbeille, la tache se supprime du tableau de liste
                 // je fais une copie de mon tableau
                 const newListe = [...maListe];
-                // je cherche dans ma newListe, s'il existe la tache faisant parti de la meme boucle
+                // je cherche dans ma newListe, s'il existe la tâche faisant parti de la meme boucle
                 if (newListe.indexOf(maListe[index]) !== -1) {
                   // si je trouve, je supprime la task du tableau copié
-                  const aSupprimer = newListe.splice(index, 1);
-                  // Puis je mets ce nouveau tableau dans le state du tableau initial
+                  newListe.splice(index, 1);
+                  // Puis je mets ce newTab sans l'objet, dans maListe
                   setMaListe(newListe);
-                  console.log(`taks deleted = ${aSupprimer}`);
-                  console.log(`ma liste = ${maListe}`);
                 }
               }}
             >
@@ -59,32 +94,22 @@ function App() {
         );
       })}
 
-      {/* Affiche mon input */}
-      <input
-        type="text"
-        placeholder="new task"
-        value={input}
-        onChange={(event) => {
-          // Je récupère la valeur de l'input
-          const newTask = event.target.value;
-          // je modifie mon state
-          setInput(newTask);
-        }}
-      />
-      {/* bouton de mon input */}
-      <button
-        type="submit"
-        onClick={() => {
-          // je crée une copie de ma liste
-          const newListe = [...maListe];
-          //  j'y ajoute ma nouvelle tâche
-          newListe.push(input);
-          // je change ma liste initiale
-          setMaListe(newListe);
-        }}
-      >
-        Add task
-      </button>
+      {/* Ajour d'une tâche */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="new task"
+          value={input}
+          onChange={(event) => {
+            // Je récupère la valeur de l'input
+            const newTask = event.target.value;
+            // je stocke la newTask dans un state
+            setInput(newTask);
+          }}
+        />
+        {/* bouton de mon input */}
+        <button type="submit">Add task</button>
+      </form>
     </div>
   );
 }
